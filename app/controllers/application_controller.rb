c5ce5b5b17046ba14ac0c58f003a9f8752d67fbe
Mako <mnfeconicu41@gmail.com>
2017-01-27 23:37:00 +0900

    response error with json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404
+    render json: { error: 404 }
+  end
+
+  def render_500
+    render json: { error: 500 }
+  end
 end
